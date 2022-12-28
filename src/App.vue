2e534ef3b72fa6f1a57321794de7e22d99f31460
sajjadnayem <18103052@iubat.edu>
2022-12-29 02:42:57 +0600

    initialized

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="min-h-screen bg-gray-100">
+    <div class="container py-6">
+      <div class="bg-white p-4 shadow rounded">
+        <div class="flex -mx-4 items-center mb-6">
+          <div class="flex-1 px-4">
+            <select class="quran-input" name="" id="">
+              <option value="">Select Sura</option>
+            </select>
+          </div>
+          <div class="flex-1 px-4 text-center">
+            <h3 class="font-bold mb-1 text-lg">Al-Baqrah</h3>
+            <p>The cow</p>
+          </div>
+
+          <div class="flex-1 px-4">
+            <select class="quran-input" name="" id="">
+              <option value="">Select Ayah</option>
+            </select>
+          </div>
+        </div>
+
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. At deserunt distinctio laboriosam nemo vero.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
